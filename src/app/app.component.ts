eb953e9166f1c13842a9175e57243adfab761bd4
dbartoszcze <dbartoszcze2@gmail.com>
2026-08-27 20:47:56 +0200

    feat: add BookService (#8)
    * add BookService and move list of books into service method
    
    * refactor: change approach of init books
    
    * refactor: Prepare service for future development
    
    * refactor: change providing and consuming book data
    
    * refactor: change way of consuming data from service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, inject, Signal} from '@angular/core';
 import {Book} from './models/book.model';
 import {BookListComponent} from './components/book-list/book-list.component';
+import {BookService} from './services/book.service';
 
 @Component({
   selector: 'app-root',
@@ -12,62 +13,8 @@ import {BookListComponent} from './components/book-list/book-list.component';
 })
 export class AppComponent {
 
-  books: Book[] = [
-    {
-      id: 1,
-      title: 'It',
-      author: 'Stephen King',
-      description:
-        'Seven children are terrorized by an ancient evil entity that takes the form of Pennywise the Dancing Clown. Years later, they reunite to confront their greatest fears.',
-      isRead: false
-    },
-    {
-      id: 2,
-      title: 'The Shining',
-      author: 'Stephen King',
-      description:
-        'Jack Torrance becomes the winter caretaker of the isolated Overlook Hotel, where supernatural forces slowly consume his sanity.',
-      isRead: true
-    },
-    {
-      id: 3,
-      title: '1984',
-      author: 'George Orwell',
-      description:
-        'A dystopian novel about a society ruled by surveillance, censorship, and the manipulation of truth by Big Brother.',
-      isRead: true
-    },
-    {
-      id: 4,
-      title: 'The Hobbit',
-      author: 'J.R.R. Tolkien',
-      description:
-        'Bilbo Baggins embarks on an unexpected adventure with a company of dwarves to reclaim their homeland from the dragon Smaug.',
-      isRead: false
-    },
-    {
-      id: 5,
-      title: 'Dune',
-      author: 'Frank Herbert',
-      description:
-        'Paul Atreides must survive on the desert planet Arrakis while becoming a central figure in a struggle over the galaxy’s most valuable resource.',
-      isRead: true
-    },
-    {
-      id: 6,
-      title: 'The Witcher: The Last Wish',
-      author: 'Andrzej Sapkowski',
-      description:
-        'A collection of stories introducing Geralt of Rivia, a monster hunter navigating a morally complex fantasy world.',
-      isRead: false
-    },
-    {
-      id: 7,
-      title: 'The Pragmatic Programmer',
-      author: 'Andrew Hunt & David Thomas',
-      description:
-        'A timeless guide for software developers covering best practices, problem-solving techniques, and professional craftsmanship.',
-      isRead: true
-    }
-  ]
+  private bookService = inject(BookService);
+
+  books:Signal<Book[]> = this.bookService.books;
+
 }
