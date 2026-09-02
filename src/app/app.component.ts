4b6db416c047a45c716fcd7b3637d1bfb9792e45
dbartoszcze <dbartoszcze2@gmail.com>
2026-09-02 19:57:33 +0200

    chore: Add eslint and prettier configuration and reformat all files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,16 @@
-import {Component, inject, Signal} from '@angular/core';
-import {Book} from './models/book.model';
-import {BookListComponent} from './components/book-list/book-list.component';
-import {BookService} from './services/book.service';
+import { Component, inject, Signal } from '@angular/core';
+import { Book } from './models/book.model';
+import { BookListComponent } from './components/book-list/book-list.component';
+import { BookService } from './services/book.service';
 
 @Component({
   selector: 'app-root',
-  imports: [
-    BookListComponent
-  ],
+  imports: [BookListComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-
   private bookService = inject(BookService);
 
-  books:Signal<Book[]> = this.bookService.books;
-
+  books: Signal<Book[]> = this.bookService.books;
 }
